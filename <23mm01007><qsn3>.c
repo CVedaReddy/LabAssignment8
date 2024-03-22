16a513deb9bcd577f0466d50a1a234b62121518d
CVedaReddy <157104274+CVedaReddy@users.noreply.github.com>
2024-03-22 12:42:26 +0530

    Update <23mm01007><qsn3>.c

--- <23mm01007><qsn3>.c
+++ <23mm01007><qsn3>.c
@@ -1,34 +1,33 @@
 #include<stdio.h>
-void rec(int i,int n,int array[n]);
+void reverse(int* start,int* end)
+{
+   if(start>=end)
+   {
+      return;
+   }
+   else
+   {
+      reverse(start+1,end-1);
+      int temp=*start;
+      *start=*end;
+      *end=temp;
+   }
+}
 int main()
 {
-    int n;
-    printf("enter number of elements in the array:");
-    scanf("%d",&n);
-    int array[n];
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",(array+i));
-    }
-    rec(1,n,array);
-    for(int i=0;i<n;i++)
-    {
-        printf("%d ",*(array+i));
-    }
-    return 0;
+   int arr[]={10,20,30,15,25};
+   int n=sizeof(arr)/sizeof(arr[0]);
+   int* start=arr;
+   int* end=arr+n-1;
+   for(int i=0;i<n;i++)
+   {
+      printf("%d\t",arr[i]);
+   }
+   reverse(start,end);
+   printf("\nrevesed:\n");
+   for(int i=0;i<n;i++)
+   {
+      printf("%d\t",arr[i]);
+   }
+
 }
-void rec(int i,int n,int array[n])
-{
-    if(i==n/2)
-    {
-        int t=*(array+i-1);
-        *(array+i-1)=*(array+n-i);
-        *(array+n-i)=t;
-    }
-    else
-    {
-        int t=*(array+i-1);
-        *(array+i-1)=*(array+n-i);
-        *(array+n-i)=t;
-        rec(i+1,n,array);
-    }}
